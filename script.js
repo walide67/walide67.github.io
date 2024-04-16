ba747fb6072523e31f3b5335c247b67a2221130b
tlemcani abdelkadir <walide67@gmail.com>
2024-04-16 12:52:54 +0100

    edit overlap

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 $(".menu-item").on("click", function(){
-    let target = $(this).attr('data-target');
-    $('.overlap').css("width", "0");
-    $('.overlap-body').css("display", "none");
-    $('.overlap-shadow').css("display", "none");
+    if($(this).hasClass('active')){
+        dismissOverlap($(this))
+    }else{
+        $('.overlap').css("width", "0");
+        $('.overlap .overlap-body').css("display", "none");
+        $('.overlap .overlap-shadow').css("display", "none");
 
-    $("#"+target).css('width', '100%');
-    $('.overlap-body', "#"+target).css('display', 'flex');
-    let timeout = setTimeout(function(){
-        $('.overlap-shadow', "#"+target).css('display', 'block');
-    }, 700);
+        let target = $(this).attr('data-target');
+        $(this).addClass('active');
+        $("#"+target).css('width', '100%');
+        $('.overlap-body', "#"+target).css('display', 'flex');
+        let timeout = setTimeout(function(){
+            $('.overlap-shadow', "#"+target).css('display', 'block');
+        }, 700);
+    }
+    
 });
 
-$(".overlap .dismiss").on("click", function(){
-    $(this).closest('.overlap').css('width', '0');
-    $(this).closest('.overlap-body').css('display', 'none');
-    $(this).closest('.overlap-shadow').css('display', 'none');
-});
+$(".overlap .dismiss").on("click", dismissOverlap($(this)));
+
+function dismissOverlap(thi){
+    $(this).removeClass('active');
+    thi.closest('.overlap').css('width', '0');
+    thi.closest('.overlap-body').css('display', 'none');
+    thi.closest('.overlap-shadow').css('display', 'none');
+}
