7d38b25753f030e19200ecbff431efd2bfc0e3c8
tlemcani abdelkadir <walide67@gmail.com>
2024-04-16 12:11:57 +0100

    edit overlap

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 $(".menu-item").on("click", function(){
     let target = $(this).attr('data-target');
-    $('.overlap').css("display", "none");
-    console.log(target)
-    $("#"+target).css('display', 'flex');
+    $('.overlap').css("width", "0");
+    $("#"+target).css('width', '100%');
 });
 
 $(".overlap .dismiss").on("click", function(){
-    $(this).closest('.overlap').css('display', 'none');
+    $(this).closest('.overlap').css('width', '0');
 });
